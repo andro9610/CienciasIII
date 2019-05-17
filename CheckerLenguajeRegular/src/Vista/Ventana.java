19d297a0e1b60939f07e7b369ac28d7d2a706b29
andro9610 <andro9610@gmail.com>
2019-05-16 21:54:12 -0500

    Estetica basica terminada
    Se termina la configuracion basica de la ventana y sus Listeners.

--- CheckerLenguajeRegular/src/Vista/Ventana.java
+++ CheckerLenguajeRegular/src/Vista/Ventana.java
@@ -1,7 +1,11 @@
 package Vista;
         
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Image;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -27,8 +31,7 @@ public class Ventana {
         Image Image = ImageIcon.getImage();
         ventana.setIconImage(Image);
         /**Añadido de paneles a la ventana*/
-        ventana.add(this.añadirElementos());
-        
+        ventana.add(añadirElementos());
         /**Mostrar la ventana*/
         ventana.setVisible(true);
     }
@@ -39,17 +42,19 @@ public class Ventana {
      * @return panel*/
     private JPanel añadirElementos(){
         JPanel panel = new JPanel();
+        panel.setLayout(null);
         /**añadido de elementos al Panel*/
-        panel.add(crearTexto("Ingrese el alfabeto, separado por comas"));
-        alfabeto = crearInput();
+        panel.add(crearTexto("Ingrese el alfabeto, separado por comas",150,30,300,30));
+        alfabeto = crearInput(200,70,200,30);
         panel.add(alfabeto);
-        panel.add(crearTexto("Ingrese la expresion regular"));
-        expresionRegular = crearInput();
-        panel.add(crearInput());
-        panel.add(crearTexto("Ingrese la cadena"));
-        cadena = crearInput();
+        panel.add(crearTexto("Ingrese la expresion regular",190,110,300,30));
+        expresionRegular = crearInput(200,150,200,30);
+        panel.add(expresionRegular);
+        panel.add(crearTexto("Ingrese la cadena",235,190,300,30));
+        cadena = crearInput(200,230,200,30);
         panel.add(cadena);
-        panel.add(crearBoton("Comprobar"));
+        panel.add(crearBoton("Comprobar",200,300,200,30));
+        
         /**Personalizacion del panel*/
         panel.setBackground(new Color(207, 209, 208));
         return panel;
@@ -59,9 +64,16 @@ public class Ventana {
      * parametros: String textoBoton
      * @return JButton;
      */
-    private JButton crearBoton(String textoBoton){
+    private JButton crearBoton(String textoBoton,int posX, int posY, int ancho, int alto){
         JButton boton = new JButton(textoBoton);
+        /**Personalizacion del boton*/
         boton.setBackground(new Color(0, 151, 123));
+        boton.setFont(new Font("Comic Sans MS", Font.PLAIN, 16));
+        boton.setBounds(new Rectangle(posX, posY, ancho, alto));
+        boton.addActionListener((ActionEvent e) -> {
+            /**Ejecucion del algoritmo*/
+            crearVentana();
+        });
         return boton;
     }
     
@@ -69,13 +81,18 @@ public class Ventana {
      * parametros: String mensaje
      * @return: JLabel;
      */
-    private JLabel crearTexto(String mensaje){
+    private JLabel crearTexto(String mensaje,int posX, int posY, int ancho, int alto){
         JLabel texto = new JLabel(mensaje);
+        texto.setFont(new Font("Comic Sans MS", Font.PLAIN, 16));
+        texto.setBounds(new Rectangle(posX, posY, ancho, alto));
         return texto;
     }
     
-    private JTextField crearInput(){
+    private JTextField crearInput(int posX, int posY, int ancho, int alto){
         JTextField campoTexto = new JTextField();
+        campoTexto.setFont(new Font("Comic Sans MS", Font.PLAIN, 16));
+        campoTexto.setBounds(new Rectangle(posX, posY, ancho, alto));
+        campoTexto.setSize(ancho, alto);
         return campoTexto;
     }
 }
