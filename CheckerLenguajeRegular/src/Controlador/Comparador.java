16c24c17c2f05d041e6242779d81a8c9b555d39f
jorgediaz96 <pbarragan56@gmail.com>
2019-05-17 19:57:46 -0500

    Update Comparador.java
    miralo perro

--- CheckerLenguajeRegular/src/Controlador/Comparador.java
+++ CheckerLenguajeRegular/src/Controlador/Comparador.java
@@ -17,7 +17,35 @@ public class Comparador {
     public static void ejecutarAlgoritmo(String alfabeto, String expresionRegular, String cadena){
         /**Codigo que llama a la funcionalidad logica del programa aqui*/
         
-        mostrarMensaje("error","Aun no existe la logica de la aplicacion");
+       // mostrarMensaje("error","Aun no existe la logica de la aplicacion");
+       String F = cadena;
+       String L = alfabeto;
+       boolean band = false;
+        int cont = 0;
+       int i=1;
+       while(i<=F.length() && band==true){
+           String letter = F.substring(cont, i);
+           int q=1;
+           int cont1=0;
+           band=false;
+           while(q<=L.length() && band==false){   
+               if(letter.equals(L.substring(cont1, q))){
+                   band=true;
+               }else{
+                   band=false;
+               }
+               cont1+=2;
+               q+=2;
+           }
+        i+=1;  
+        cont+=1;   
+       }
+       if(band==false){
+           System.out.println("No pertenece al lenguaje");
+       }else{
+           System.out.println("Pertenece al lenguaje");
+       }
+    }
     }
     
     /**Metodos que permiten pasar informacion de la capa logica a la capa vista
