827e6bbb3a9758d06c5e00dfad4c6014c26f2fcb
jorgediaz96 <pbarragan56@gmail.com>
2019-05-18 11:21:39 -0500

    Documentacion
    Verificar la total documentacion

--- CheckerLenguajeRegular/src/Logica/Comparador.java
+++ CheckerLenguajeRegular/src/Logica/Comparador.java
@@ -8,25 +8,25 @@ import java.util.regex.Pattern;
 public class Comparador {
     /**Metodos que permiten pasar datos de la Capa Vista a la capa logica*/
 
-
+/**ejecutarAlgoritmo: Metodo dedicado a la comprobacion de cadenas de texto.
+     * parametros: String alfabetoIngresado, String expresionRegularIngresada,
+     * String cadenaIngresada,boolean Band, boolean exist,int cont, int i, int q,
+     * int cont1, Pattern patron, Matcher m, Salida s1.
+     * @return void;
+     */
     public static void ejecutarAlgoritmo(String alfabetoIngresado, 
             String expresionRegularIngresada, String cadenaIngresada){
-        
-       String expresionRegular = expresionRegularIngresada;
-       String cadena = cadenaIngresada;
-       String alfabeto = alfabetoIngresado;
        boolean band = true, exist=false;
-
 
         int cont = 0;
         int i=1;
-        while(i<=cadena.length() && band==true){
-            String letter = cadena.substring(cont, i);
+        while(i<=cadenaIngresada.length() && band==true){
+            String letter = cadenaIngresada.substring(cont, i);
             int q=1;
             int cont1=0;
             band=false;
-            while(q<=alfabeto.length() && band==false){   
-                if(letter.equals(alfabeto.substring(cont1, q))){
+            while(q<=alfabetoIngresado.length() && band==false){   
+                if(letter.equals(alfabetoIngresado.substring(cont1, q))){
                     band=true;
                 }else{
                     band=false;
@@ -48,8 +48,8 @@ public class Comparador {
         }
         /**Hace comprobacion de coincidencia entre expresion logica y la frase ingresada*/
         if(exist == true){
-            Pattern patron = Pattern.compile(expresionRegular);
-            Matcher m = patron.matcher(cadena);
+            Pattern patron = Pattern.compile(expresionRegularIngresada);
+            Matcher m = patron.matcher(cadenaIngresada);
             boolean coincidencia = m.matches();
             Salida s1;
             if(coincidencia==true){
